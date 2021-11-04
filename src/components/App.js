34ffdcd64703d2c47339481eff3b1ea306f2cbad
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-11-05 05:46:24 +0800

    mudulize max numbering

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Gift from './Gift';
+import { max_number } from '../helper';
 
 class App extends Component {
     constructor() {
@@ -16,9 +17,7 @@ class App extends Component {
 
         const ids = this.state.gifts.map(gift => gift.id);
 
-        const max_id = ids.length > 0 ? Math.max(...ids) : 0;
-
-        gifts.push({ id: max_id + 1 });
+        gifts.push({ id: max_number(ids) + 1 });
 
         this.setState({ gifts: gifts });
     }
